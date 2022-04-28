935c7eece42662c499f93331826d14af26106ade
Arona NGOM <arona.ngom07@gmail.com>
2022-04-28 18:58:41 +0200

    edit admin controller

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,14 +19,17 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(NewsCrudController::class)->generateUrl());
 
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Asa App');
+       //     ->setTitle('Asa App')
+            ->setTitle('<img src="public/upload/images/news/actu.png"> Admin ASA');
     }
 
     public function configureMenuItems(): iterable
--- src/Controller/Admin/NewsCrudController.php
+++ src/Controller/Admin/NewsCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\News;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -19,6 +22,27 @@ class NewsCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return News::class;
+    }
+
+
+    public function configureActions(Actions $actions): Actions
+    {
+
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::NEW, function(Action $action){
+                return $action->setIcon('fa fa-newspaper')->addCssClass('btn btn-success');
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function(Action $action){
+            return $action->setIcon('fa fa-edit')->addCssClass('btn btn-warning');
+        })
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function(Action $action){
+                return $action->setIcon('fa fa-trash')->addCssClass('btn btn-danger text-white');
+            })
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function(Action $action){
+                return $action->setIcon('fa fa-eye')->addCssClass('btn btn-light');
+            });
     }
 
 
