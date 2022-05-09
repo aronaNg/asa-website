03b61cf58b4ea9b253ee866044682cfc33617de4
Arona NGOM <arona.ngom07@gmail.com>
2022-05-09 16:59:46 +0200

    import bootstrap icons

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
 import 'bootstrap';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
+
