35643e70698fb507802906fb4cb0acbbf120c5ca
Arona NGOM <arona.ngom07@gmail.com>
2022-05-18 17:19:50 +0200

    🔨fix team crud

---
+++ src/Controller/Admin/TeamCrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Team;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class TeamCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Team::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            ImageField::new('image',"image")
+                ->setBasePath('/upload/images/team/')
+                ->setUploadDir('/public/upload/images/team/'),
+            TextField::new('name'),
+            TextEditorField::new('entitled'),
+            TextareaField::new('imageFile')
+                ->setFormType(VichImageType::class)
+                ->hideOnIndex(),
+
+            DateField::new('updatedAt')->hideOnForm(),
+
+        ];
+    }
+
+}
