f2052d3428a448374b00b4f76db149a899cec598
Arona NGOM <arona.ngom07@gmail.com>
2022-05-24 17:51:31 +0200

    upload image

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -12,7 +12,7 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -20,6 +20,6 @@ class UserCrudController extends AbstractCrudController
             TextField::new('title'),
             TextEditorField::new('description'),
         ];
-    }*/
+    }
 
 }
